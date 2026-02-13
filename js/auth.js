8abbd84592507c321a5dfb0ab569dd1650447b7f
arulkumar17042007 <e.arulkumar50@gmail.com>
2026-02-14 01:08:54 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,40 +1,31 @@
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-
 async function signup() {
-  const email = emailInput.value.trim();
-  const password = passwordInput.value.trim();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-  if (!email || !password) {
-    alert("Enter email and password");
-    return;
-  }
-
-  const { data, error } = await supabaseClient.auth.signUp({
+  const { error } = await supabaseClient.auth.signUp({
     email: email,
     password: password
   });
 
   if (error) {
-    alert("Signup Error: " + error.message);
+    alert(error.message);
   } else {
     alert("Signup successful! Now login.");
   }
 }
 
 async function login() {
-  const email = emailInput.value.trim();
-  const password = passwordInput.value.trim();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-  const { data, error } = await supabaseClient.auth.signInWithPassword({
+  const { error } = await supabaseClient.auth.signInWithPassword({
     email: email,
     password: password
   });
 
   if (error) {
-    alert("Login Error: " + error.message);
+    alert(error.message);
   } else {
-    alert("Login successful!");
     window.location.href = "model.html";
   }
 }
