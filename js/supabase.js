b87d28701456bf627cbcbbaa0beb41d6db758900
arulkumar17042007 <e.arulkumar50@gmail.com>
2026-02-13 23:40:56 +0530

    Add Supabase client initialization in supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+// js/supabase.js
+
+const supabaseUrl = "https://aphdefijlpnyrqahcbtt.supabase.co";
+const supabaseKey = "sb_publishable_j_HM2l4xtFcEpFcNg1LJMA_NdtiyfRy";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
